87bbad9c72e1df388fcf997d4361a6b20f4eb6d3
nursh <nsheikhmohammed@yahoo.com>
2019-01-12 08:19:13 -0500

    Finished with search button

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from "react";
 
+
+import sprite from '../img/sprite.svg';
+
 class App extends Component {
+
+
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    alert("Hagabadla");
+  }
+
   render() {
     return (
       <div className="app">
         <div className="heading">
           <h1>Toronto Waste Lookup</h1>
         </div>
+        <form className="search" onSubmit={this.handleSubmit}>
+          <input type="text" className="search__input" />
+          <button className="search__button" type="submit">
+            <svg className="search__icon">
+              <use xlinkHref={`${sprite}#icon-search`} />
+            </svg>
+          </button>
+        </form>
+        
       </div>
     );
   }
